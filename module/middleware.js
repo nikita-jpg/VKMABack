12d99a298a54b9464b5bd5d36c2d61c1d49c23ef
Nikita <nicitazaletin@yandex.ru>
2021-08-31 00:31:52 +0300

    Оптимизация кода

--- module/middleware.js
+++ module/middleware.js
@@ -1,29 +1,27 @@
-const { log } = require("winston");
-const sign = require("./sign");
-
-module.exports =  {
-    async execute(fastify, command, request, reply) {
-      try {
-        const user_data = sign(request.headers.authorization);
-
-        // был инициализирован после проверки подписи
-        let id = user_data?.vk_user_id;
-        
-        
-        let _root = command?.root; // true
-        if (command?.auth) {
-            // Получение данных о пользователе с бд или любого хранилища ... 
-            let user = { name: "Artur Frank", root: 1 };
-            //
-            
-            if (!_root || user.role == _root) { request.body = { ...{ id: id }, ...request.body }; }
-            else { return fastify.response.All(403, { message: `403 Forbidden` }, reply); }
-        }
-        else {return fastify.response.All(400, { message: `400 Bad Request` }, reply);}
+module.exports = {
+  async execute(fastify, command, request, reply) {
+    console.log(request)
+    try {
+      // Получение id из объекта sign который 
+      // был инициализирован после проверки подписи
+      let id = request?.sign.vk_user_id;
+      //
+      let _auth = command?.auth; // true
+      let _root = command?.root; // true
+      console.log(id)
+      if (id && _auth) {
+          // Получение данных о пользователе с бд или любого хранилища ... 
+          let user = { name: "Artur Frank", root: 1 };
+          //
+          
+          if (!_root || user.role == _root) { request.body = { ...{ id: id }, ...request.body }; }
+          else { return fastify.response.All(403, { message: `403 Forbidden` }, reply); }
       }
-      catch (error) {
-        fastify.log.error(error);
-        return fastify.response.All(500, { message: `500 Internal Server Error` }, reply);
-      }
-   }
+      else {return fastify.response.All(400, { message: `400 Bad Request` }, reply);}
+    }
+    catch (error) {
+      fastify.log.error(error);
+      return fastify.response.All(500, { message: `500 Internal Server Error` }, reply);
+    }
+ }
 }
