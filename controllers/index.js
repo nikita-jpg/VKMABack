9f50acec4e1b2b7edff6a6a7f26a57f82d991ace
Nikita <nicitazaletin@yandex.ru>
2022-05-15 17:37:57 +0300

    Начальный коммит

--- controllers/index.js
+++ controllers/index.js
@@ -1,29 +1,29 @@
 // 10 запросов в 5 секунд
 const config = {
-    rateLimit: {
-        max: 10,
-        timeWindow: 5000
-    }
-}
+  rateLimit: {
+    max: 10,
+    timeWindow: 5000,
+  },
+};
 
-const bd = require('../module/workWithDB');
+const bd = require("../module/workWithDB");
 module.exports = {
-    method: "GET",
-    config: config,
-    async execute(fastify, request, reply) {
-        try {
-            const userId = Number(request.body.id);
+  method: "GET",
+  config: config,
+  async execute(fastify, request, reply) {
+    console.log("I am work");
+    try {
+      const userId = Number(request.body.id);
 
-            if(isNaN(userId)){
-                return fastify.response.All(403, "Access denied", reply)
-            }
+      if (isNaN(userId)) {
+        return fastify.response.All(403, "Access denied", reply);
+      }
 
-            const req = await bd.getStartDate(userId)
-            return fastify.response.All(200, req, reply)
-        }
-        catch (error) {
-            console.log(error)
-            fastify.response.All(500, "This is very bad =(", reply)
-        }
+      const req = await bd.getStartDate(userId);
+      return fastify.response.All(200, req, reply);
+    } catch (error) {
+      console.log(error);
+      fastify.response.All(500, "This is very bad =(", reply);
     }
-}
+  },
+};
--- index.js
+++ index.js
@@ -1,116 +1,126 @@
-const sign = require('./module/sign'); // импорт функции проверки подписи.
-const Redis = require('ioredis'); // импорт либы для использования redis.
-const redis = new Redis({
-    connectionName: "RAID_LIMIT",
-    host: "localhost",
-    port: +6379,
-    connectTimeout: +500
-})
+const sign = require("./module/sign"); // импорт функции проверки подписи.
+const Redis = require("ioredis"); // импорт либы для использования redis.
 
-const fastify = require('fastify')({
-    logger: {
-        level: 'info',
-        file: `log/${new Date().getFullYear()}_${new Date().getMonth()}_${new Date().getDay()}.log`
-    }
+const redis = new Redis({
+  connectionName: "RAID_LIMIT",
+  host: "localhost",
+  port: +6379,
+  connectTimeout: +500,
 });
 
-fastify.register(require('fastify-cors'), {
-    origin: "*",
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Methods']
-})
+const fastify = require("fastify")({
+  logger: {
+    level: "info",
+    file: `log/${new Date().getFullYear()}_${new Date().getMonth()}_${new Date().getDay()}.log`,
+  },
+});
 
-fastify.register(require('fastify-formbody'));
+fastify.register(require("fastify-cors"), {
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Access-Control-Allow-Origin",
+    "Access-Control-Allow-Methods",
+  ],
+});
 
-fastify.register(require('fastify-rate-limit'),
-    {
-        global: false, // Не используем глобальные лимиты.
-        redis: redis, // Используем redis для хранения данных о лимитах.
-        keyGenerator: (request) => {
-            // Проверяем подпись параметров запуска.
-            // Параметры передаются в header в параметре Authorization
-            let param = sign(request.headers.authorization); 
-            if (param) {
-                request.sign = param;
-                return param.vk_user_id;
-            } else { 
-                return "undefined" 
-            }
-        },
-        errorResponseBuilder: () => { return undefined },
-    });
+fastify.register(require("fastify-formbody"));
 
-// Автоматический рутинг, основанный на файлах и папках
-fastify.register(require('fastify-easy-route'), {
-    path: "controllers",
-    middleware: './module/middleware'
+fastify.register(require("fastify-rate-limit"), {
+  global: false, // Не используем глобальные лимиты.
+  redis: redis, // Используем redis для хранения данных о лимитах.
+  keyGenerator: (request) => {
+    // Проверяем подпись параметров запуска.
+    // Параметры передаются в header в параметре Authorization
+    let param = sign(request.headers.authorization);
+    console.log(request.headers);
+    if (param) {
+      request.sign = param;
+      return param.vk_user_id;
+    } else {
+      return "undefined";
+    }
+  },
+  errorResponseBuilder: () => {
+    return undefined;
+  },
 });
 
 // Сокращает ответ до fastify.response.All(код, ответ, reply)
-fastify.register(require('./module/response'));
+fastify.register(require("./module/response"));
 
 // Рукописный плагин для работы с бд
-fastify.register(require('./module/mysql'));
+fastify.register(require("./module/mysql"));
 
 // Для отправки статики
-const path = require('path');
-fastify.register(require('fastify-static'), {
-    root: path.join(__dirname, 'images'), // images - расположение папки со статикой отнносительно папки с текущим файлом (то есть с папки с index.js)
-})
+const path = require("path");
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "images"), // images - расположение папки со статикой отнносительно папки с текущим файлом (то есть с папки с index.js)
+});
 
-fastify.register(
-    require('sequelize-fastify'),
-    {
-      instance: 'sequelize',
-      sequelizeOptions: {
-        dialect: "mysql",
-        database: "vkma_db",
-        username: "root",
-        password: "toor",
-        host: "127.0.0.1",
-        port: "3307",
-        logging: false,
-        define: {
-            timestamps: false
-          }
-      }
-    }
-  )
-.ready(async () => {
+fastify
+  .register(require("sequelize-fastify"), {
+    instance: "sequelize",
+    sequelizeOptions: {
+      dialect: "mysql",
+      database: "vkma_db",
+      username: "root",
+      password: "toor",
+      host: "127.0.0.1",
+      port: "3306",
+      logging: false,
+      define: {
+        timestamps: false,
+      },
+    },
+  })
+  .ready(async () => {
     try {
-    // Инициализация моделей
-    const initModels = require('./module/workWithDB');
-    try{
+      // Инициализация моделей
+      const initModels = require("./module/workWithDB");
+      try {
         initModels.initModels(fastify);
-        fastify.sequelize.sync().then(result=>{
+        fastify.sequelize
+          .sync()
+          .then((result) => {
             // console.log(result);
-            })
-            .catch(err=> console.log(err));
-    }catch(err){
-        console.log(err)
+          })
+          .catch((err) => console.log(err));
+      } catch (err) {
+        console.log(err);
+      }
+    } catch (err) {
+      console.log(err);
     }
-    } catch(err) {
-    console.log(err)
-    }
-})
+  });
 
+// fastify.get("/", async (request, reply) => {
+//   return { hello: "world" };
+// });
 
+// // Автоматический рутинг, основанный на файлах и папках
+fastify.register(require("fastify-easy-route"), {
+  path: "controllers",
+  middleware: "./module/middleware",
+});
 
 // Функция запуска сервера
 const start = () => {
-    try {
-        fastify.listen(18301, "0.0.0.0", (error) => {
-            if (error) {
-                fastify.log.error(error);
-                process.exit(1);
-            }
-            console.log(`Server listening on http://127.0.0.1:18301`)
-        })
-    }
-    catch (error) {
+  try {
+    fastify.listen(18301, "0.0.0.0", (error) => {
+      if (error) {
         fastify.log.error(error);
-        process.exit(1)
-    }
-}
+        process.exit(1);
+      }
+      console.log(`Server listening on http://127.0.0.1:18301`);
+    });
+  } catch (error) {
+    console.log(error);
+    fastify.log.error(error);
+    process.exit(1);
+  }
+};
 // Запускаем сервер
-start()
+start();
